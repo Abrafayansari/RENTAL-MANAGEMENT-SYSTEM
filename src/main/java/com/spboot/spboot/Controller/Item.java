e792d254f884a9ef49ab9e059e362f4210b8bd22
TalalMajid <talalmajid558@gmail.com>
2025-04-26 01:13:09 +0500

    Made classes

--- src/main/java/com/spboot/spboot/Controller/Item.java
+++ src/main/java/com/spboot/spboot/Controller/Item.java
@@ -1,4 +1,57 @@
 package com.spboot.spboot.Controller;
 
 public class Item {
+    protected long itemId;
+    protected String itemName;
+    protected String category;
+    protected double pricePerDay;
+    protected boolean isAvailable;
+
+    public Main(long itemId, String itemName, String category, double pricePerDay, boolean isAvailable) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.category = category;
+        this.pricePerDay = pricePerDay;
+        this.isAvailable = isAvailable;
+    }
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
--- src/main/java/com/spboot/spboot/Controller/Listings.java
+++ src/main/java/com/spboot/spboot/Controller/Listings.java
@@ -1,4 +1,47 @@
 package com.spboot.spboot.Controller;
 
 public class Listings extends Item{
+    private long listingId;
+    private Item item;
+    private User owner;
+    private String description;
+
+    public Main(long listingId, Item item, User owner, String description) {
+        this.listingId = listingId;
+        this.item = item;
+        this.owner = owner;
+        this.description = description;
+    }
+
+    public long getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(long listingId) {
+        this.listingId = listingId;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
