20ab2e38933ce1cbf3798bf81714459b3cbadf79
TalalMajid <talalmajid558@gmail.com>
2025-04-26 15:59:20 +0500

    Made 3 classes

---
+++ BACKEND/src/main/java/RMS/Classes/Property.java
@@ -0,0 +1,48 @@
+package RMS.Classes;
+
+public class Property extends Item {
+    private String address;
+    private int numberOfRooms;
+    private double areaInSquareFeet;
+    private boolean furnished;
+
+    public Property(long itemId, String itemName, double pricePerDay, String address, int numberOfRooms, double areaInSquareFeet, boolean furnished) {
+        super(itemId, itemName, "Property", pricePerDay);
+        this.address = address;
+        this.numberOfRooms = numberOfRooms;
+        this.areaInSquareFeet = areaInSquareFeet;
+        this.furnished = furnished;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public double getAreaInSquareFeet() {
+        return areaInSquareFeet;
+    }
+
+    public void setAreaInSquareFeet(double areaInSquareFeet) {
+        this.areaInSquareFeet = areaInSquareFeet;
+    }
+
+    public boolean isFurnished() {
+        return furnished;
+    }
+
+    public void setFurnished(boolean furnished) {
+        this.furnished = furnished;
+    }
+}
