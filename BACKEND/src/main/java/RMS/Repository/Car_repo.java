ee3c371e87cc3ffde33d08cb33e6f44d71e90fa2
TalalMajid <talalmajid558@gmail.com>
2025-05-01 23:22:38 +0500

    Search functionality

--- BACKEND/src/main/java/RMS/Repository/Car_repo.java
+++ BACKEND/src/main/java/RMS/Repository/Car_repo.java
@@ -3,5 +3,8 @@ package RMS.Repository;
 import RMS.Classes.Car;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface Car_repo extends MongoRepository<Car,Long> {
+    List<Car> findByItemNameContainingIgnoreCase(String itemName);
 }
--- BACKEND/src/main/java/RMS/Repository/Property_repo.java
+++ BACKEND/src/main/java/RMS/Repository/Property_repo.java
@@ -1,7 +1,11 @@
 package RMS.Repository;
 
+import RMS.Classes.Clothing;
 import RMS.Classes.Property;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface Property_repo extends MongoRepository<Property,Long> {
+    List<Property> findByItemNameContainingIgnoreCase(String itemName);
 }
